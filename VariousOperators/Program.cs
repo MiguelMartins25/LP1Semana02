9eb7026be1c00b03bfe5d44dfc01b8e428f18426
Miguel Martins <miguelarbmartins@gmail.com>
2026-02-26 15:48:37 +0000

    Exercicio 4

---
+++ VariousOperators/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace VariousOperators
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a non-negative number:");
+            string number = Console.ReadLine();
+            int n = int.Parse(number);
+            int ndevide = n / 2;
+            Console.WriteLine(ndevide);
+            int nshift = n << 3;
+            Console.WriteLine(nshift);
+            int nXOR = n ^ 6;
+            Console.WriteLine(nXOR);
+            bool nsize = n > 10;
+            Console.WriteLine(nsize);
+        }
+    }
+}
