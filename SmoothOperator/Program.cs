f0ac4c3ec4145c007beeb72e8f7912c1236f9f92
Miguel Martins <miguelarbmartins@gmail.com>
2026-02-26 15:28:00 +0000

    Exercício 1

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a number");
+            string number = Console.ReadLine();
+            sbyte n = sbyte.Parse(number);
+            sbyte nplus = n++;
+            Console.WriteLine(nplus);
+            sbyte nminus = n--;
+            Console.WriteLine(nminus);
+        }
+    }
+}
