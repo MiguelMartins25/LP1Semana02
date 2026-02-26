79478d4fd13b0b23495bd214cd50e546d7c87f9d
Miguel Martins <miguelarbmartins@gmail.com>
2026-02-26 16:45:03 +0000

    Added conversion

--- TempConvert/Program.cs
+++ TempConvert/Program.cs
@@ -10,8 +10,25 @@ namespace TempConvert
             CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
             Console.Write("Temperature: ");
             string temperature = Console.ReadLine();
+            double temperaturedub = double.Parse(temperature);
             Console.Write("Unit (C/F): ");
             string unit = Console.ReadLine();
+            switch (unit)
+            {
+                case "C":
+                    double temperatureconv = temperaturedub * 1.8 + 32;
+                    Console.WriteLine(temperaturedub + " C = " + temperatureconv + " F");
+                    break;
+
+                case "F":
+                    double temperatureconv2 = (temperaturedub - 32)/1.8;
+                    Console.WriteLine(temperaturedub + " F = " + temperatureconv2 + " C");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid unit.");
+                    break;
+            }
         }
     }
 }
